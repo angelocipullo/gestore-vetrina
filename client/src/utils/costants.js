f21cd4033e5fb648cb5751c863a20125ce7b73f3
Angelo Cipullo <angelo.cipullo@outlook.it>
2022-10-21 01:04:28 +0200

    no message

--- client/src/utils/costants.js
+++ client/src/utils/costants.js
@@ -1,6 +1,6 @@
 import events from './eventType'
 import address from './serverAddress.json'
 
-export const SERVER_URL = `ws://${(window.origin === 'localhost') ? 'localhost': window.origin }:8000`
+export const SERVER_URL = `ws://${(window.origin === 'localhost') ? 'localhost': window.location.hostname }:8000`
 export const EVENTS = events
 
