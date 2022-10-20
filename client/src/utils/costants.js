2ec673fdc3b8cb01d04094361574d0214fe1a3e7
Angelo Cipullo <angelo.cipullo@outlook.it>
2022-10-21 00:42:12 +0200

    Dynamic server address

--- client/src/utils/costants.js
+++ client/src/utils/costants.js
@@ -1,5 +1,6 @@
 import events from './eventType'
+import { address } from './serverAddress.json'
 
-export const SERVER_URL = `ws://${(window.origin === 'localhost') ? window.origin : '192.168.1.100'}:8000`
+export const SERVER_URL = `ws://${(window.origin === 'localhost') ? window.origin : address }:8000`
 export const EVENTS = events
 
